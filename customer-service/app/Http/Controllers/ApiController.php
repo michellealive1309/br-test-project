11410a87fd6517b7f4cc50a2b48808bb49124e95
Pitiphat <michellealive1309@gmail.com>
2026-04-13 20:56:43 +0700

    Add api controller for being api document base

---
+++ customer-service/app/Http/Controllers/ApiController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use OpenApi\Attributes as OA;
+
+#[OA\Info(
+    version: "1.0.0",
+    title: "Customer API Documentation",
+    description: "Manage your account and customer"
+)]
+class ApiController extends Controller
+{
+    //
+}
